67f284e958a6d8b8096a60a3c7371d8d1f85d57a
malves <malves@10.180.40.170>
2015-11-24 12:06:36 -0300

    criei uma classe de eventos; modifiquei a janela para trabalhar com evento;

--- Projeto01/src/projeto01/Projeto01.java
+++ Projeto01/src/projeto01/Projeto01.java
@@ -5,18 +5,41 @@
  */
 package projeto01;
 
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Panel;
+
 /**
  *
  * @author malves
  */
-public class Projeto01 {
+public class Projeto01 extends Frame {
 
+    public Projeto01() {
+        setVisible(true);
+        setSize(300, 300);
+        setTitle("Projeto 01");
+        
+        SeClicarNoBotao scnb = new SeClicarNoBotao();
+        
+        Panel p = new Panel();
+        
+        Button b = new Button("botão");
+        b.addActionListener(scnb);
+        
+        p.add(b);
+        
+        add(p);
+        
+    }
+
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("ola mundo");
+        new Projeto01();
     }
     
 }
---
+++ Projeto01/src/projeto01/SeClicarNoBotao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto01;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author malves
+ */
+public class SeClicarNoBotao implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("eu cliquei na tela");
+    }
+    
+}
